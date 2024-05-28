6fe1b75869ca05c71c3dac76930e8bcee8f8be7a
nicolasmarra <nicolasmarra12@gmail.com>
2024-05-28 10:24:29 +0200

    suppression des fichiers générés par doxygen

--- html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['_3cimg_20src_3d_22assets_2ficon_2epng_22_20alt_3d_22icon_22_20width_3d_224_25_22_2f_3e_20casse_2dbriques_20_2d_20un_20remake_1',['&lt;img src=&quot;assets/icon.png&quot; alt=&quot;icon&quot; width=&quot;4%&quot;/&gt; Casse-Briques - Un remake',['../md_README.html',1,'']]]
-];
